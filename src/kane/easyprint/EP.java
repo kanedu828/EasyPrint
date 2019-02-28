6103674e0fc528428b52374042d44fce376ad59e
kanedu828 <kanedu2000@gmail.com>
2019-02-27 23:44:43 -0500

    First commit!

---
+++ src/kane/easyprint/EP.java
@@ -0,0 +1,11 @@
+package kane.easyprint;
+
+public class EP {
+    static <T> void print(T obj){
+        System.out.print(obj);
+    }
+    static <T> void println(T obj){
+        System.out.println(obj);
+    }
+
+}
---
+++ src/kane/easyprint/TestDriver.java
@@ -0,0 +1,10 @@
+package kane.easyprint;
+
+public class TestDriver{
+    public static void main(String[] args){
+        EP.println("hi");
+        EP.println("there");
+        EP.print("yeet");
+        EP.print("YOOT");
+    }
+}
